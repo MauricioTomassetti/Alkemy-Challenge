81fef1542b4b73081bd1fef8094e72b1e588ee58
Mauri88 <mauricio.tomassetti@gmail.com>
2020-11-05 19:37:20 -0300

    Exclude a route reset from Auth routes.

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/', function () {
 
 Route::resource('posts', PostController::class);
 
-Auth::routes();
+Auth::routes(['reset' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
