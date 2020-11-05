7331707a088d3e0844a84bc7b4612f0ecd6ade81
Mauri88 <mauricio.tomassetti@gmail.com>
2020-11-05 12:35:32 -0300

    Make a resource Route and Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,8 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::resource('posts', PostController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
