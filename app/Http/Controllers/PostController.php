0132334ae7739df363142203e44c534aca7a5f40
Mauri88 <mauricio.tomassetti@gmail.com>
2020-11-05 12:25:27 -0300

    Create a functions (show, create, store, edit, update, destroy, validatePost)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -12,5 +12,58 @@ class PostController extends Controller
         $posts = Post::latest()->get();
 
         return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.detail', ['post' => $post, 'category' => $post->category()->first()->name]);
+    }
+
+    public function create(Category $category)
+    {
+        return view('posts.create', ['categories' => $category->all()]);
+    }
+
+    public function store()
+    {
+        $path = request('image')->store('images');
+
+        $post = Post::create($this->validatePost());
+
+        $post->image = @end(explode('/', $path));
+        $post->save();
+
+        return redirect('/posts');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update(Post $post)
+    {
+        $post->update(request()->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]));
+
+        return redirect('posts');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('posts');
+    }
+
+    public function validatePost()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'category' => 'required',
+            'image' => 'required|file|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
     }
 }
