a9896fde6a04bd2a2dfbc44c1d2f251e0277dba3
Mauri88 <mauricio.tomassetti@gmail.com>
2020-11-06 09:17:08 -0300

    Ordening code

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::resource('posts', PostController::class);
 
 Auth::routes(['reset' => false]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
